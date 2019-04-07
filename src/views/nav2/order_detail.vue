93bd475d3353d01d6e9d1d439786b892122d6b75
FlashSquirrel <1061283711@qq.com>
2019-04-07 20:54:39 +0800

    调整订单列表页及详情页的展示数据

--- src/views/nav2/order_detail.vue
+++ src/views/nav2/order_detail.vue
@@ -10,51 +10,95 @@
                 <span class="title">订单详情</span>
             </div>
             <div class="text item">
-                <span class="orderInfo">用户名：{{detail.buyuserno}}</span>
+                <span class="orderInfo" v-if="lotnoType=='JC'">用户名：{{detail.buyuserno}}</span>
+                <span class="orderInfo" v-if="lotnoType=='SZC'">备注名：{{detail.buyuserno}}</span>
                 <span class="orderInfo">订单编号：{{detail.id}}</span>
-                <span class="orderInfo">金额：{{detail.amt}}</span>
+                <span class="orderInfo">金额：￥{{detail.amt}}[{{detail.lotmulti}}倍]</span>
                 <span class="orderInfo">{{detail.orderstate}}</span>
             </div>
             <div>
                 <el-card class="box-card" shadow="never" >
                     <div slot="header" class="clearfix">
                         <span class="title subTitle">{{detail.lotnoName}}</span>
-                        <span class="title subTitle">{{detail.teamCnt}}场</span>
-                        <span class="title subTitle">{{detail.subDesc?detail.subDesc+"串1":""}}</span>
+                        <span class="title subTitle" v-if="lotnoType=='JC'">{{detail.teamCnt}}场</span>
+                        <span class="title subTitle" v-if="lotnoType=='JC'">{{detail.subDesc?detail.subDesc+"串1":""}}</span>
+                        <span class="title subTitle" v-if="lotnoType=='SZC'">第{{detail.batchcode}}期</span>
                     </div>
-                    <el-table
-                            :data="tableData"
-                            style="width: 100%">
-                        <el-table-column
-                                prop="week"
-                                label="场次">
-                        </el-table-column>
-                        <el-table-column
-                                prop="enddate"
-                                label="截止时间">
-                        </el-table-column>
-                        <el-table-column
-                                label="对阵">
-                            <template slot-scope="scope">
-                                <span class="subTitle">{{scope.row.team[0]}}</span>
-                                <span class="subTitle">VS{{scope.row.peilvs.peilv?'('+scope.row.peilvs.peilv+')':""}}</span>
-                                <span class="subTitle">{{scope.row.team[1]}}</span>
-                            </template>
-                        </el-table-column>
-                        <el-table-column label="投注项">
-                            <template slot-scope="scope">
-                                <el-button
-                                        size="mini"
-                                        type="danger">让负 1640</el-button>
-                                <el-button
-                                        size="mini">大 1750</el-button>
-                            </template>
-                        </el-table-column>
-                        <el-table-column
-                                prop="result"
-                                label="彩果">
-                        </el-table-column>
-                    </el-table>
+                    <div v-if="lotnoType=='JC'">
+                        <el-table
+                                :data="tableData"
+                                style="width: 100%">
+                            <el-table-column
+                                    prop="week"
+                                    align="center"
+                                    width="120"
+                                    label="场次">
+                            </el-table-column>
+                            <el-table-column
+                                    prop="enddate"
+                                    align="center"
+                                    width="120"
+                                    label="截止时间">
+                            </el-table-column>
+                            <el-table-column
+                                    align="center"
+                                    label="对阵">
+                                <template slot-scope="scope">
+                                    <span class="subTitle">{{scope.row.team[0]}}</span>
+                                    <span class="subTitle">VS{{scope.row.peilvs[0].peilv?'('+scope.row.peilvs[0].peilv+')':""}}</span>
+                                    <span class="subTitle">{{scope.row.team[1]}}</span>
+                                </template>
+                            </el-table-column>
+                            <el-table-column
+                                    align="center" label="投注项">
+                                <template slot-scope="scope">
+                                    <!--<el-button-->
+                                    <!--size="mini"-->
+                                    <!--type="danger">让负 1640</el-button>-->
+                                    <el-button size="mini" v-for="peilv in scope.row.peilvs">
+                                        {{peilv.type | peilvTypeFilter}}
+                                        <span v-if="peilv.peilv">{{peilv.peilv}}</span>
+                                    </el-button>
+                                </template>
+                            </el-table-column>
+                            <el-table-column
+                                    align="center"
+                                    prop="result"
+                                    width="120"
+                                    label="彩果">
+                            </el-table-column>
+                        </el-table>
+                    </div>
+                    <div v-if="lotnoType=='SZC'">
+                        <el-table
+                                :data="tableData"
+                                style="width: 100%">
+                            <el-table-column
+                                    prop="batchcode"
+                                    align="center"
+                                    width="180"
+                                    label="期号">
+                            </el-table-column>
+                            <el-table-column
+                                    prop="lotno"
+                                    align="center"
+                                    width="120"
+                                    label="玩法">
+                            </el-table-column>
+                            <el-table-column
+                                    prop="betcode"
+                                    align="center"
+                                    label="投注号码">
+                            </el-table-column>
+                            <el-table-column
+                                    align="center"
+                                    prop="result"
+                                    width="120"
+                                    label="彩果">
+                            </el-table-column>
+                        </el-table>
+                    </div>
+
                 </el-card>
             </div>
         </el-card>
@@ -68,24 +112,27 @@
         data() {
             return {
                 detail:null,
-                tableData: [{
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1517 弄'
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1519 弄'
-                }, {
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1516 弄'
-                }]
+                tableData: []
             }
+        },
+        filters:{
+            lotnoType:null,
+            peilvTypeFilter:function (peilv){
+                let peilvTypes = {
+                        "v0":"主负",
+                        "v1":"平",
+                        "v3":"主胜",
+                        "v1 v3":"主胜 平",
+                        "v1 v0":"主胜 主负",
+                        "v0 v3":"主胜 主负",
+                        "v0 v1":"平 主负",
+                        "letVs_v0":"让负",
+                        "letVs_v":"让平",
+                        "g":"大",
+                        "l":"小"
+                        };
+              return peilvTypes[peilv]?peilvTypes[peilv]:peilv;
+          }
         },
         methods: {
             handleEdit(index, row) {
@@ -104,9 +151,10 @@
             let detail = self.$route.params.detail;
             let type = self.$route.params.type;
             let week = {"1":"周一","2":"周二","3":"周三","4":"周四","5":"周五","6":"周六","7":"周日"};
-            console.info(detail)
-            if(type == 3){
-                //撤单类型
+            console.info(detail.pid)
+            self.lotnoType = detail.pid;
+            if(detail.pid == "JC"){
+                //jigncai类型
                 if(detail.orderInfo){
                     let info = detail.orderInfo.split("@")[0]
                     detail.subDesc =  info.substr(info.length -1,info.length)
@@ -120,10 +168,19 @@
 
                 detail.teamCnt = jingcai.length;
                 self.tableData = jingcai;
+            }else if(detail.pid == "GPC"){
+                //gaopingcai
 
-
+            }else if(detail.pid == "SZC"){
+                //数字彩
+                let data = {
+                    lotno:detail.lotno,
+                    batchcode:detail.batchcode,
+                    betcode:detail.betcode,
+                    result:detail.result
+                }
+                self.tableData = [data]
             }
-
             self.detail = detail;
 
         }
