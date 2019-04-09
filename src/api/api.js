62784fc12abac02339405368ec522d50150bf855
liyangli <ylisoftware@163.com>
2019-04-10 00:37:22 +0800

    调整后台接口访问方式

--- src/api/api.js
+++ src/api/api.js
@@ -2,25 +2,25 @@ import axios from 'axios';
 
 let base = '';
 
-export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
-export const requestRegister = params => { return axios.post(`${base}/register`, params).then(res => res.data); };
+export const requestLogin = params => { return axios.post(`${base}/api/login`, params).then(res => res.data); };
+export const requestRegister = params => { return axios.post(`${base}/api/register`, params).then(res => res.data); };
 
-export const getUserList = params => { return axios.post(`${base}/betUser/search`, params).then(res => res.data); };
-export const addBetUser = params => { return axios.post(`${base}/betUser/add`, params).then(res => res.data); };
+export const getUserList = params => { return axios.post(`${base}/api/betUser/search`, params).then(res => res.data); };
+export const addBetUser = params => { return axios.post(`${base}/api/betUser/add`, params).then(res => res.data); };
 
-export const findOrders = params => { return axios.post(`${base}/order/search`, params).then(res => res.data); };
-export const findTypes = params => { return axios.get(`${base}/order/findType`,{ params: params }).then(res => res.data); };
+export const findOrders = params => { return axios.post(`${base}/api/order/search`, params).then(res => res.data); };
+export const findTypes = params => { return axios.get(`${base}/api/order/findType`,{ params: params }).then(res => res.data); };
 
-export const getUserListPage = params => { return axios.get(`${base}/user/listpage`, params); };
+export const getUserListPage = params => { return axios.get(`${base}/api/user/listpage`, params); };
 
-export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
+export const removeUser = params => { return axios.get(`${base}/api/user/remove`, { params: params }); };
 
-export const batchRemoveUser = params => { return axios.get(`${base}/user/batchremove`, { params: params }); };
+export const batchRemoveUser = params => { return axios.get(`${base}/api/user/batchremove`, { params: params }); };
 
-export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
+export const editUser = params => { return axios.get(`${base}/api/user/edit`, { params: params }); };
 
-export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
+export const addUser = params => { return axios.get(`${base}/api/user/add`, { params: params }); };
 
 //获取注册用户统计数据
-export const findUserStatistics = params => { return axios.get(`${base}/statistics/findUserTotalDataMutation`, { params: params }); };
-export const doQueryStatistics = (type,params) => { return axios.post(`${base}/`+type+"/statistics", params); };
+export const findUserStatistics = params => { return axios.get(`${base}/api/statistics/findUserTotalDataMutation`, { params: params }); };
+export const doQueryStatistics = (type,params) => { return axios.post(`${base}/api/`+type+"/statistics", params); };
